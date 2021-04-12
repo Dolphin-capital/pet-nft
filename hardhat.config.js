eb6114dc773d5e28bcafbaa08f0e4644ad2ec8e5
jackf5299 <jackf5299@gmail.com>
2021-04-12 19:09:05 +0800

    add .gitignore

---
+++ hardhat.config.js
@@ -0,0 +1,136 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("dotenv/config")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-deploy")
+require("hardhat-watcher")
+require("hardhat-deploy-ethers")
+require("hardhat-gas-reporter")
+require("hardhat-typechain")
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+
+const accounts = {
+  mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk",
+  // accountsBalance: "990000000000000000000",
+}
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: "hardhat",
+  mocha: {
+    timeout: 750000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasPrice: 120 * 1000000000,
+      timeout:1000000,
+      chainId: 1,
+    },
+    localhost: {
+      url: `http://127.0.0.1:7545`,
+      live: false,
+      gasPrice: 1000000000,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 3,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 4,
+      gasPrice: 1500000000,
+      live: true,
+      saveDeployments: true,
+      timeout:1000000,
+      tags: ["staging"],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 5,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 42,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+    },
+    hecotest: {
+      url: "https://http-testnet.hecochain.com",
+      accounts,
+      chainId: 256,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+    },
+    heco: {
+      url: "https://http-mainnet-node.huobichain.com",
+      accounts,
+      chainId: 128,
+      live: true,
+      gasPrice: 1400000000,
+      saveDeployments: true,
+      timeout:1000000,
+      tags: ["staging"],
+    },
+  },
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+  watcher: {
+    compile: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+    },
+  },
+};
